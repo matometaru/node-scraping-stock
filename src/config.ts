0d5b2efcc0c947298c703131427cf55e7272052b
Ryosuke <>
2018-10-20 13:37:40 +0900

    ignore

--- src/config.ts
+++
@@ -1,6 +0,0 @@
-// ダウンロード先のリンク
-export const PARSE_URL = 'https://kabuoji3.com/stock/';
-// ダウンロード先のリンク
-export const DOWNLOAD_URL = 'https://kabuoji3.com/stock/file.php';
-// ダウンロードの間隔(ms)
-export const DELAY = 2000;
